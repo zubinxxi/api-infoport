cf2da825410ec4f9062878234d9c4ea51f4bddae
Zubin Juárez <zubinxxi@gmail.com>
2026-01-28 15:29:21 -0500

    se actualizó en archivo main para configurar la página de la ruta /docs

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 from routes.home import router as router_home
 from routes.movies import router as movies_router
 
+# 1. Desactivar docs predeterminados al crear la app
+#app = FastAPI(docs_url=None, redoc_url=None)
 app = FastAPI()
-app.title = "Mi API con FastAPI"
+
+# Montar carpeta estática
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.title = "API - INFOPORT"
 app.version = "1.0.0"
 app.description = "Esta es una API de ejemplo creada con FastAPI"
-app.license_info = {
-    "name": "MIT License",
-    "url": "https://opensource.org/licenses/MIT",
-}
+
 
 app.openapi_tags = [
     {
@@ -21,5 +25,20 @@ app.openapi_tags = [
         "description": "Rutas relacionadas con películas",
     },                                  
 ]
+
+app.swagger_ui_parameters = {
+    "syntaxHighlight": {"theme": "obsidian",}
+}
+
+#@app.get("/docs", include_in_schema=False)
+#async def custom_swagger_ui_html():
+#    return get_swagger_ui_html(
+#        openapi_url=app.openapi_url,
+#        title="Mi API con FastAPI",
+#        # 3. Define aquí la URL de tu icono (relativa o absoluta)
+#        swagger_favicon_url="/static/favicon.jpg", 
+#        # Si usas local: swagger_favicon_url="/static/favicon.png"
+#    )
+
 app.include_router(router_home)
 app.include_router(movies_router)
